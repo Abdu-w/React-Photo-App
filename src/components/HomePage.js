e2e99cf55e6bdd5f974a2b402cc11a07e54e4595
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-26 21:02:26 -0400

    add comments

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 export default class HomePage extends Component{
     state = {
+      //when the user is logged in stay true else false
       logedIn  : false,
       upload: []
     }
