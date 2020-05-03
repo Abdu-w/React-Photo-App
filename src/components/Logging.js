29596918b7cd718a41d156d21a89cd27a87d394e
Abdu-w <61803409+Abdu-w@users.noreply.github.com>
2020-05-03 19:13:34 -0400

    Update Logging.js

--- src/components/Logging.js
+++ src/components/Logging.js
@@ -31,8 +31,7 @@ export default class Logging extends Component {
     }
     
     
-    //use bracket notaion to matchh input's  name props  and state[keys]
-    handleChange = (e) => this.setState({[e.target.name]: e.target.value})
+
     
     render() {
       return (
