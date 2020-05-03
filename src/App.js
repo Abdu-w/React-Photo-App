0d0c22360f635bb11aa02ff4363d4e1fe0d8fedb
Abdu-w <61803409+Abdu-w@users.noreply.github.com>
2020-05-03 18:52:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch,Link} from 'react-router-dom'
 import Logging from './components/Logging'
 import HomePage from './components/HomePage'
-import './App.css';
+
 
 
 
