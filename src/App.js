8d4b159f44f60fba82bcd089e42b3f263f148931
Abdu-w <61803409+Abdu-w@users.noreply.github.com>
2020-05-03 18:59:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch,Link} from 'react-router-dom';
-import Logging from './components/Logging';
-import HomePage from './components/HomePage';
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Switch,Link} from 'react-router-dom'
+import Logging from './components/Logging'
+import HomePage from './components/HomePage'
 import './App.css';
+
+
 
 
 function App() {
   return (
-   
+    <BrowserRouter>
+      <div className="App">
+
+        <Switch>  
+          <Route path="/home" component={HomePage}/>
+          <Route component={Logging}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
