1b2d85ab4b8e77c3c4736915fca5a7f059c51197
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-05-03 22:47:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch,Link} from 'react-router-dom'
 import Logging from './components/Logging'
 import HomePage from './components/HomePage'
